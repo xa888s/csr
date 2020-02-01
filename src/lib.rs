9cd5f07174358ad63f95725ec8fff845b4cb6aa2
abyss <derribado@tutanota.com>
2020-01-31 17:53:05 -0800

    Removed heap allocations by directly mutating input

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use num::cast::AsPrimitive;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 #[derive(Clone, Copy)]
 pub struct Caesar {
@@ -8,55 +8,47 @@ pub struct Caesar {
 
 impl Caesar {
     pub fn new<U: AsPrimitive<u8>>(shift: U) -> Self {
-        // Shift size must be bigger than 0 and smaller than or equal to 26
+        // shift size must be bigger than 0 and smaller than or equal to 26
         match shift.as_() {
             0..=26 => Caesar { shift: shift.as_() },
             _ => panic!("Shift size must be between 0 and 26!"),
         }
     }
 
-    pub fn encrypt<S: Deref<Target = str>>(&self, buf: S) -> String {
-        let chars = buf.as_bytes();
+    pub fn encrypt<S: Deref<Target = str> + DerefMut>(self, buf: &mut S) {
+        let chars = unsafe { buf.as_bytes_mut() };
 
-        let vec: Vec<u8> = chars
-            .iter()
-            .map(|c| match c {
+        for c in chars {
+            *c = match c {
                 65..=90 => {
-                    let pos = c % 65;
+                    let pos = *c % 65;
                     65 + ((pos + self.shift) % 26)
                 }
                 97..=122 => {
-                    let pos = c % 97;
+                    let pos = *c % 97;
                     97 + ((pos + self.shift) % 26)
                 }
                 _ => *c,
-            })
-            .collect();
-
-        // this is safe because non-utf8 bytes will never be pushed to vec
-        unsafe { String::from_utf8_unchecked(vec) }
+            }
+        }
     }
 
-    pub fn decrypt<S: Deref<Target = str>>(&self, buf: S) -> String {
-        let chars = buf.as_bytes();
+    pub fn decrypt<S: Deref<Target = str> + DerefMut>(self, buf: &mut S) {
+        let chars = unsafe { buf.as_bytes_mut() };
 
-        let vec: Vec<u8> = chars
-            .iter()
-            .map(|c| match c {
+        for c in chars {
+            *c = match c {
                 65..=90 => {
-                    let pos = c % 65;
+                    let pos = *c % 65;
                     90 - (((25 - pos) + self.shift) % 26)
                 }
                 97..=122 => {
-                    let pos = c % 97;
+                    let pos = *c % 97;
                     122 - (((25 - pos) + self.shift) % 26)
                 }
                 _ => *c,
-            })
-            .collect();
-
-        // this is safe because non-utf8 bytes will never be pushed to vec
-        unsafe { String::from_utf8_unchecked(vec) }
+            }
+        }
     }
 }
 
@@ -69,10 +61,11 @@ mod tests {
         let key: u8 = 10;
         let caesar = Caesar::new(key);
 
-        let input = String::from("Drsc sc k coxdoxmo");
+        let mut input = String::from("Drsc sc k coxdoxmo");
+        caesar.decrypt(&mut input);
         let output = String::from("This is a sentence");
 
-        assert_eq!(caesar.decrypt(input), output);
+        assert_eq!(input, output);
     }
 
     #[test]
@@ -80,10 +73,11 @@ mod tests {
         let key: u8 = 20;
         let caesar = Caesar::new(key);
 
-        let input = String::from("Tests are important");
+        let mut input = String::from("Tests are important");
+        caesar.encrypt(&mut input);
         let output = String::from("Nymnm uly cgjilnuhn");
 
-        assert_eq!(caesar.encrypt(input), output);
+        assert_eq!(input, output);
     }
 
     #[test]
@@ -91,10 +85,11 @@ mod tests {
         let key: u8 = 15;
         let caesar = Caesar::new(key);
 
-        let input = String::from("😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍");
+        let mut input = String::from("😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍");
+        caesar.decrypt(&mut input);
         let output = input.clone();
 
-        assert_eq!(caesar.decrypt(input), output);
+        assert_eq!(input, output);
     }
 
     #[test]
@@ -102,31 +97,10 @@ mod tests {
         let key: u8 = 15;
         let caesar = Caesar::new(key);
 
-        let input = String::from("😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍");
+        let mut input = String::from("😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍");
+        caesar.encrypt(&mut input);
         let output = input.clone();
 
-        assert_eq!(caesar.encrypt(input), output);
-    }
-
-    #[test]
-    fn str_test() {
-        let key: u8 = 2;
-        let caesar = Caesar::new(key);
-
-        let input = "Hello world!";
-        let output = "Jgnnq yqtnf!".to_string();
-
-        assert_eq!(caesar.encrypt(input), output);
-    }
-
-    #[test]
-    fn slice_test() {
-        let key: u8 = 2;
-        let caesar = Caesar::new(key);
-
-        let input = "Top secret message!";
-        let output = "Vqr ugetgv".to_string();
-
-        assert_eq!(caesar.encrypt(&input[0..10]), output);
+        assert_eq!(input, output);
     }
 }
